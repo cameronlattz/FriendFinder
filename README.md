242b9ab2b5b33c55b9ef44219af035b5a4f502ca
Cameron Lattz <camerolattz@gmail.com>
2019-09-24 21:01:43 -0500

    adding styling, minor fixes

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # FriendFinder
+blah
+
+### OVERVIEW:
+blah
+
+### HOW TO RUN:
+Run server.js in node with the following structure:
+```
+    node .\server.js
+```
+
+### VIDEO:
+[![Video](https://img.youtube.com/vi/X62zeL3pWww/0.jpg)](https://www.youtube.com/watch?v=X62zeL3pWww)
+
+### TECHNOLOGIES USED:
+* Node
+* Express Node Module
+* Heroku
+
+### TEAM:
+* Cameron Lattz, Developer
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -10,16 +10,21 @@ module.exports = function(app) {
     }
 
     app.get("/api/friends", function(req, res) {
-        readFile(res.send);
+        readFile(function(friends) {
+            res.send(friends);
+        });
     });
 
     app.post("/api/friends", function(req, res) {
         const user = req.body;
         readFile(function(friends) {
+            // setting a default friend in case none are found
             let closestFriend = {
                 name: "No friends added yet.",
                 photo: "https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half/public/blogs/148105/2014/07/154399-158210.png",
             };
+            // setting the difference between scores as the highest number possible so any other friend besides default
+            // will have a lower score
             let closestFriendDifference = Number.MAX_SAFE_INTEGER;
             friends.forEach(friend => {
                 let difference = 0;
